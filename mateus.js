ab2135f0a2071d7d24979692e5c610fb37eced89
Mateussgueler <87372169+Mateussgueler@users.noreply.github.com>
2021-07-13 20:29:19 -0300

    Primeiro Commit

---
+++ mateus.js
@@ -0,0 +1,17 @@
+alert('Oi, alerta java script')
+
+confirm('Alerta de confirmação')
+
+let valorConfirm = confirm('Alerta de confirmação guardando o valor')
+alert(valorConfirm)
+
+let txt = "";
+
+txt += "Cookies Enabled: " + navigator.cookieEnabled + "\n";
+txt += "Browser Language: " + navigator.language + "\n";
+txt += "Browser Oline: " + navigator.online + "\n";
+txt += "Plataform: " + navigator.plataform + "\n";
+
+alert(txt);
+
+
